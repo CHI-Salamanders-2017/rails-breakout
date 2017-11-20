d7523d473cd1b98264905e4ee7ecc3bdc9440214
Alyssa Diaz <alydiaz77@gmail.com>
2017-11-20 12:42:24 -0600

    breakout one complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create]
+  resources :songs, except: [:destroy]
+
+  # resources :categories, only: [:index] do
+  #   resources :articles, except: [:destroy]
+  # end
+
+  # get '/rounds/:id/next_card', to: 'rounds#next_card', as: "next_card"
+  get '/logout', to: 'sessions#destroy'
+
+  root 'songs#index'
 end
